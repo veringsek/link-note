32e25605b327f41ec2f737c1d358dfc1cbb636ea
veringsek <veringsek@gmail.com>
2022-04-17 16:59:57 +0800

    import data

--- options.js
+++ options.js
@@ -4,5 +4,20 @@ function exportData() {
     });
 }
 
+function importData() {
+    let txtData = document.getElementById('txtData');
+    let data;
+    try {
+        data = JSON.parse(txtData.value);
+    } catch (error) {
+        console.log(error);
+    }
+    chrome.storage.local.clear(() => {
+        chrome.storage.local.set(data, () => {
+            document.getElementById('txtMessage').innerHTML = 'done';
+        });
+    });
+}
+
 document.getElementById('btnExport').addEventListener('click', exportData);
-document.getElementById('btnImport').addEventListener('click', exportData);
+document.getElementById('btnImport').addEventListener('click', importData);
