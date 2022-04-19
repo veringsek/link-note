faeb1a9c55049b449d2c04c77915790aec8e3403
veringsek <veringsek@gmail.com>
2022-04-20 07:35:25 +0800

    link-note-link only shows when right click on page

--- service_worker.js
+++ service_worker.js
@@ -24,7 +24,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
             chrome.contextMenus.create({
                 id: `link-note-page--${type.type}`,
                 title: `${chrome.i18n.getMessage('noteThisPage')} - ${type.type}`,
-                contexts: ['all'],
+                contexts: ['page'],
                 targetUrlPatterns: [
                     '*://*/*'
                 ]
